9caed71b7c8a5e461220df8b6e0d5a0090ac3d95
ErkinA25 <erkina2@nycstudents.net>
2026-02-13 09:37:01 -0500

    jhk

--- app.py
+++ app.py
@@ -3,14 +3,35 @@ import json
 pokedex = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
-print(data[0])
 
 # Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
-
 # Add a language choice feature and print the pokemons name based on the user input
+def print_pokemon_names(data):
+    lan_choice = input("What laguage do you want: Japanese, English, French, or Chinese? ")
+    x = lan_choice.capitalize()
+    if x == "Japanese":
+        for pokemon in data:
+            print(pokemon["name"]["japanese"])
+    elif x == "English":
+        for pokemon in data:
+            print(pokemon["name"]["english"])
+    elif x == "French":
+        for pokemon in data:
+            print(pokemon["name"]["french"])
+    elif x == "Chinese":
+        for pokemon in data:
+            print(pokemon["name"]["chinese"])
+print_pokemon_names(data)
 
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-
+def type_list(data):
+    type_choice = input("What pokemon type are you searching for? ")
+    x = type_choice.capitalize()
+    for type_choice in data["name"]:
+        print(data["name"])
+    else:
+        print("No pokemon of that type was found.")
+type_list(data)
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
 
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
